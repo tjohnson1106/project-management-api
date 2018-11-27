a0d15e2524c60646bd3ab16f1beb722cc4db15aa
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-27 13:23:43 -0500

    config

--- lib/project_management_example_web/channels/user_socket.ex
+++ lib/project_management_example_web/channels/user_socket.ex
@@ -1,10 +1,10 @@
 defmodule ProjectManagementExampleWeb.UserSocket do
   use Phoenix.Socket
 
-  ...
-
   ## Transports
   socket "/socket", HelloWeb.UserSocket,
     websocket: [timeout: 45_000],
     longpoll: false
+
+  def id(_socket), do: nil
 end
