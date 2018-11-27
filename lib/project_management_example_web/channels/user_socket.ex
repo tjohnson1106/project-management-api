2f1f371b9de6210ee9245d8c61cef8336e82b21e
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-27 13:36:48 -0500

    altered socket method

--- lib/project_management_example_web/channels/user_socket.ex
+++ lib/project_management_example_web/channels/user_socket.ex
@@ -1,10 +1,14 @@
 defmodule ProjectManagementExampleWeb.UserSocket do
   use Phoenix.Socket
 
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+
   ## Transports
   socket "/socket", HelloWeb.UserSocket,
     websocket: [timeout: 45_000],
     longpoll: false
-
-  def id(_socket), do: nil
 end
